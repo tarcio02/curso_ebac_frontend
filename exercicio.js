cc4a0077124b9467d3baae54b6d1f8858e256cd5
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-01-03 15:34:07 -0200

    v1.0

---
+++ exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicaNum(altura, base) {
+    return altura * base;
+}
+;
+function dizOla(nome) {
+    return 'olá' + nome;
+}
+;
+multiplicaNum(6, 8);
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicaNum(altura: number, base: number ): number {
+    return altura * base;
+};
+
+function dizOla(nome:string):string {
+    return 'olá' + nome;
+};
+
+
+multiplicaNum(6, 8);
+dizOla("tarcio");
